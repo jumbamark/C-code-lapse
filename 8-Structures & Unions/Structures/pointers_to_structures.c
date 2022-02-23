65f89cebe494450cd2339d45837aa09d7fc8cfbc
Mark Jumba <jumbamark@yahoo.com>
2022-02-23 12:26:11 +0300

    Add pointer declaration

--- 8-Structures & Unions/Structures/pointers_to_structures.c
+++ 8-Structures & Unions/Structures/pointers_to_structures.c
@@ -22,10 +22,24 @@ void main () {
     showStudentData(&st1);
 }
 
+// Declaring a pointer to struct variable ship and accessing its members
+struct ship {
+    int x;
+    int y;
+} sh1;
+
+void pointer () {
+    struct ship *ptr = &sh1;
+    ptr->x = 3;
+    ptr->y = 4;
+}
+
 /*
  * Just like pointers to variables, pointers to structures can also be defined
  * struct myStruct *struct_ptr;  (defines a pointer to the myStruct structure.)
  * struct_ptr = &struct_var (stores the address of the structure variable struct_var in the pointer struct_ptr).
  * struct_ptr -> struct_mem  (accesses the value of the structure member struct_mem)
  * When typedef has been used to name the struct a pointer is declared using only the typedef name along with * and the pointer name.
+ * 
+
 */
